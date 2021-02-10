1aab6f89f66d27c53607614a1444280517e031d3
Leslie Book <lesliek.book@gmail.com>
2021-02-10 11:51:09 -0500

    Final Commit, everything working

--- templates/TeamRoster.js
+++ templates/TeamRoster.js
@@ -6,8 +6,7 @@ class TeamRoster {
             `
         <!doctype html>
         <html lang="en">
-        <head>
-        <!-- Required meta tags -->
+        <head>        
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
         <!-- Bootstrap CSS -->
@@ -46,8 +45,7 @@ class TeamRoster {
         ${this.team}
         </div>
         </div>
-        <!-- Optional JavaScript -->
-        <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+        
         <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js"
             integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo"
             crossorigin="anonymous"></script>
